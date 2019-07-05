3ee5f5a95c532aada85004bb8c470ec93a33b9a0
HoangNguyen0524 <40507081+HoangNguyen0524@users.noreply.github.com>
2019-07-05 11:02:00 -0700

    Update sketch.js

--- Final-Project2/sketch.js
+++ Final-Project2/sketch.js
@@ -1,4 +1,3 @@
-    
 function setup()
 {  
    createCanvas(800, 400);
@@ -9,11 +8,23 @@ function setup()
 function draw()
 {
     drawCircle();
+    drawFace();
 }
 
 function drawCircle()
 {
+    fill('grey');
     background('black');
     ellipse(mouseX, mouseY, 25, 25);
     
+}
+
+function drawFace()
+{
+    fill(229, 194, 152);
+    ellipse(775, 200, 100, 200);
+    triangle(720, 185, 730, 160, 725, 185);
+    fill('black');
+    ellipse(725, 235, 50, 50);
+    ellipse(765, 150, 10, 20);
 }
